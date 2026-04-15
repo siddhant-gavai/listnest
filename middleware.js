7a0b062190841951412cbd712b8878f8975fa4da
Siddhant Gavai <siddhantgavai0@gmail.com>
2026-04-15 19:39:36 +0530

    Enhance middleware robustness with existence checks

--- middleware.js
+++ middleware.js
@@ -23,6 +23,10 @@ module.exports.saveRedirectUrl = (req, res, next) => {
 module.exports.isOwner = async (req, res, next) => {
   let { id } = req.params;
   let listing = await Listing.findById(id);
+  if (!listing) {
+    req.flash("error", "Listing not found!");
+    return res.redirect("/listings");
+  }
   if (!listing.owner._id.equals(res.locals.currUser._id)) {
     req.flash("error", " you are not the owner of this listing  ");
     return res.redirect(`/listings/${id}`);
@@ -54,8 +58,12 @@ module.exports.isReviewAuthor = async (req, res, next) => {
   const { id, reviewId } = req.params;
   const review = await Review.findById(reviewId);
 
+  if (!review) {
+    req.flash("error", "Review not found!");
+    return res.redirect(`/listings/${id}`);
+  }
+
   if (
-    !review ||
     !review.author ||
     !res.locals.currUser ||
     !review.author.equals(res.locals.currUser._id)
