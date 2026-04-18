7c776920328811ebcd6b92e1ac673b44368b8c2b
Siddhant Gavai <siddhantgavai0@gmail.com>
2026-04-18 13:07:11 +0530

    refactor: improve controller consistency and error handling

--- controllers/user.js
+++ controllers/user.js
@@ -14,7 +14,7 @@ module.exports.signUp = async (req, res) => {
       if (err) {
         return next(err);
       }
-      req.flash("success", "Welcome to Wanderlust 😍😍");
+      req.flash("success", "Welcome to Listnest! 😍");
       res.redirect("/listings");
     });
   } catch (e) {
@@ -28,7 +28,7 @@ module.exports.renderLoginForm = (req, res) => {
 };
 
 module.exports.login = async (req, res) => {
-  req.flash("success", "Welcome to Wanderlust 😍😍");
+  req.flash("success", "Welcome back to Listnest! 😍");
   let redirectUrl = res.locals.redirectUrl || "/listings";
   res.redirect(redirectUrl);
 };
